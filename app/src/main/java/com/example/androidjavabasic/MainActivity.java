12787cd5b07d263973307e38207189c7d544d398
snehalsonawale222 <snehal.sonawale@firstcry.com>
2020-05-02 11:26:34 +0530

    only commit

--- app/src/main/java/com/example/androidjavabasic/MainActivity.java
+++ app/src/main/java/com/example/androidjavabasic/MainActivity.java
@@ -1,21 +1,65 @@
 package com.example.androidjavabasic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     String val;
 
 
-
+    private RecyclerView recyclerView;
+    private PlanetAdapter adapter;
+    private ArrayList<Planet> planetArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        val="np";
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        planetArrayList = new ArrayList<>();
+        adapter = new PlanetAdapter(this, planetArrayList);
+        recyclerView.setAdapter(adapter);
+        createListData();
+
+
+    }
+
+    private void createListData() {
+        Planet planet = new Planet("Earth", 150, 10, 12750);
+        planetArrayList.add(planet);
+        planet = new Planet("Jupiter", 778, 26, 143000);
+        planetArrayList.add(planet);
+        planet = new Planet("Mars", 228, 4, 6800);
+        planetArrayList.add(planet);
+        planet = new Planet("Pluto", 5900, 1, 2320);
+        planetArrayList.add(planet);
+        planet = new Planet("Venus", 108, 9, 12750);
+        planetArrayList.add(planet);
+        planet = new Planet("Saturn", 1429, 11, 120000);
+        planetArrayList.add(planet);
+        planet = new Planet("Mercury", 58, 4, 4900);
+        planetArrayList.add(planet);
+        planet = new Planet("Neptune", 4500, 12, 50500);
+        planetArrayList.add(planet);
+        planet = new Planet("Uranus", 2870, 9, 52400);
+        planetArrayList.add(planet);
+
+        Log.e("planetArrayListttt",""+planetArrayList);
+       // Log.e("planet data",""+planet.toString());
+       // System.out.println("planet value data"+planet);
+
+        adapter.notifyDataSetChanged();
+
+
     }
 }
---
+++ app/src/main/java/com/example/androidjavabasic/PlanetAdapter.java
@@ -0,0 +1,85 @@
+package com.example.androidjavabasic;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.ViewHolder> {
+
+
+    private Context context;
+    private ArrayList<Planet> planets;
+
+    public PlanetAdapter( Context context, ArrayList<Planet> planets) {
+
+        this.context = context;
+        this.planets = planets;
+    }
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+
+        View view = LayoutInflater.from(context).inflate(R.layout.itemrow, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Planet planet = planets.get(position);
+        Log.e("lost size",""+planet);
+        holder.setDetails(planet);
+        
+        
+
+       holder.txtName.setText(planets.get(position).getPlanetName());
+
+        holder.txtDistance.setText(String.format(Locale.US, "Distance from Sun : %d Million KM", planets.get(position).getDistanceFromSun()));
+        holder. txtGravity.setText(String.format(Locale.US, "Surface Gravity : %d N/kg", planets.get(position).getGravity()));
+        holder. txtDiameter.setText(String.format(Locale.US, "Diameter : %d KM", planets.get(position).getDiameter()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return planets.size();
+
+
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtName, txtDistance, txtGravity, txtDiameter;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtName = itemView.findViewById(R.id.txtName);
+            txtDistance = itemView.findViewById(R.id.txtDistance);
+            txtGravity = itemView.findViewById(R.id.txtGravity);
+            txtDiameter = itemView.findViewById(R.id.txtDiameter);
+        }
+
+
+        public void setDetails(Planet planet) {
+
+            //txtName.setText(planet.getPlanetName());
+           // txtDistance.setText(String.format(Locale.US, "Distance from Sun : %d Million KM", planet.getDistanceFromSun()));
+           // txtGravity.setText(String.format(Locale.US, "Surface Gravity : %d N/kg", planet.getGravity()));
+            //txtDiameter.setText(String.format(Locale.US, "Diameter : %d KM", planet.getDiameter()));
+        }
+    }
+
+
+}
